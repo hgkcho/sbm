709f3df9a436f3c71e4425bcf91a6b7fd03e7190
hgkcho <pulpul538552@gmail.com>
2021-05-03 18:27:30 +0900

    parse go token

--- main.go
+++ main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
+	"go/scanner"
+	"go/token"
 	"os"
 	"strings"
 )
@@ -20,12 +21,55 @@ var ignoreWords = []string{
 }
 
 func main() {
-	s := bufio.NewScanner(os.Stdin)
-	s.Scan()
-	t := s.Text()
+	// sc := bufio.NewScanner(os.Stdin)
+	// sc.Scan()
+	// t := sc.Text()
+
+	// var q = "select u.name from users as u join post as p on u.id = p.user_id" +
+	// 	"where user_id = ?"
+
+	const t = `
+	var query = "select u.name from users as u" +
+		" join post as p join p.user_id = u.id "
+	`
+
 	ret := prs(t)
 
 	fmt.Fprintln(os.Stdout, ret)
+}
+
+func prs(input string) string {
+	var ret string
+	fset := token.NewFileSet()
+	var s scanner.Scanner
+	file := fset.AddFile("", fset.Base(), len(input))
+	s.Init(file, []byte(input), nil, scanner.ScanComments)
+
+LOOP:
+	for {
+		_, tok, lit := s.Scan()
+		switch tok {
+		case token.EOF:
+			break LOOP
+		case token.SEMICOLON:
+			continue
+		case token.ASSIGN:
+			ret = ret + " " + token.ASSIGN.String()
+		case token.DEFINE:
+			ret = ret + " " + token.DEFINE.String()
+		case token.ADD:
+			ret = ret + " " + token.ADD.String()
+		case token.STRING:
+			// remove double quotation from lit
+			lit = lit[1 : len(lit)-1]
+			s := prsString(lit)
+			lit = "\"" + s + "\""
+			ret = ret + " " + lit
+		default:
+			ret = ret + " " + lit
+		}
+	}
+	return ret
 }
 
 func surround(input string) string {
@@ -54,8 +98,12 @@ func surround(input string) string {
 	return ret + commaBuf
 }
 
-func prs(input string) string {
+func prsString(input string) string {
 	var ret string
+	var hasSpace bool
+	if strings.HasPrefix(input, " ") {
+		hasSpace = true
+	}
 	// sp := strings.Split(input, " ")
 	sp := strings.Fields(input)
 	for i, v := range sp {
@@ -71,5 +119,53 @@ func prs(input string) string {
 			ret += " " + buf
 		}
 	}
+	if hasSpace {
+		ret = " " + ret
+	}
 	return ret
+}
+
+func tmp() error {
+	// var query = "select u.name from users as u" +
+	// " join post as p join p.user_id = u.id "
+
+	// const src = `var query = "select u.name from users as u"`
+	const src = `
+	var query = "select u.name from users as u" +
+		" join post as p join p.user_id = u.id "
+	`
+
+	var ret string
+	fset := token.NewFileSet()
+	var s scanner.Scanner
+	file := fset.AddFile("", fset.Base(), len(src))
+	s.Init(file, []byte(src), nil, scanner.ScanComments)
+
+LOOP:
+	for {
+		_, tok, lit := s.Scan()
+		// ret = ret + " " + lit
+		switch tok {
+		case token.EOF:
+			break LOOP
+		case token.ASSIGN:
+			ret = ret + " " + token.ASSIGN.String()
+		case token.DEFINE:
+			ret = ret + " " + token.DEFINE.String()
+		case token.ADD:
+			ret = ret + " " + token.ADD.String()
+		case token.STRING:
+			tok.IsLiteral()
+			// remove double quotation from lit
+			lit = lit[1 : len(lit)-1]
+			s := surround(lit)
+			lit = "\"" + s + "\""
+			ret = ret + " " + lit
+		default:
+			ret = ret + " " + lit
+		}
+	}
+
+	fmt.Println(ret)
+	return nil
 }
--- main_test.go
+++ main_test.go
@@ -6,7 +6,57 @@ import (
 	"github.com/google/go-cmp/cmp"
 )
 
-func Test_parse(t *testing.T) {
+var query = "SELECT `id`, `name` FROM `users` AS `u` JOIN `articles` AS `a` ON `u`.`id` = `a`.`user_id`"
+
+func Test_prsString(t *testing.T) {
+	t.Parallel()
+	tests := []struct {
+		name  string
+		input string
+		want  string
+	}{
+		{
+			name:  "ok_1",
+			input: "select id, name from users",
+			want:  "select `id`, `name` from `users`",
+		},
+		{
+			name:  "ok_2",
+			input: "select id, name from users as u join articles as a on u.id = a.user_id",
+			want:  "select `id`, `name` from `users` as `u` join `articles` as `a` on `u`.`id` = `a`.`user_id`",
+		},
+		{
+			name:  "ok_3",
+			input: "SELECT id, name FROM users ",
+			want:  "SELECT `id`, `name` FROM `users`",
+		},
+		{
+			name:  "ok_4",
+			input: "SELECT id, name FROM users AS u JOIN articles AS a ON u.id = a.user_id",
+			want:  "SELECT `id`, `name` FROM `users` AS `u` JOIN `articles` AS `a` ON `u`.`id` = `a`.`user_id`",
+		},
+		{
+			name:  "ok_5",
+			input: "  select id, name from users ",
+			want:  " select `id`, `name` from `users`",
+		},
+		{
+			name:  "ok_6",
+			input: "\tselect id, name from users ",
+			want:  "select `id`, `name` from `users`",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := prsString(tt.input)
+			if diff := cmp.Diff(tt.want, got); diff != "" {
+				t.Errorf("mismatch (-want +got) %s\n", diff)
+			}
+		})
+	}
+}
+
+func Test_prs(t *testing.T) {
 	t.Parallel()
 	tests := []struct {
 		name  string
@@ -15,40 +65,23 @@ func Test_parse(t *testing.T) {
 	}{
 		{
 			name:  "ok",
-			input: "select id, name from users",
-			want:  "select `id`, `name` from `users`",
+			input: `var q = "select u.id, u.name from user as u"`,
+			want:  " var q = \"select `u`.`id`, `u`.`name` from `user` as `u`\"",
 		},
 		{
-			name:  "ok",
-			input: "select id, name from users as u join articles as a on u.id = a.user_id",
-			want:  "select `id`, `name` from `users` as `u` join `articles` as `a` on `u`.`id` = `a`.`user_id`",
-		},
-		{
-			name:  "ok",
-			input: "SELECT id, name FROM users ",
-			want:  "SELECT `id`, `name` FROM `users`",
-		},
-		{
-			name:  "ok",
-			input: "SELECT id, name FROM users AS u JOIN articles AS a ON u.id = a.user_id",
-			want:  "SELECT `id`, `name` FROM `users` AS `u` JOIN `articles` AS `a` ON `u`.`id` = `a`.`user_id`",
-		},
-		{
-			name:  "ok",
-			input: "  select id, name from users ",
-			want:  "select `id`, `name` from `users`",
-		},
-		{
-			name:  "ok",
-			input: "\tselect id, name from users ",
-			want:  "select `id`, `name` from `users`",
+			name: "ok",
+			input: `var q = "select u.id, u.name from user as u" +
+			" join post as p on u.id = p.user_id where u.id = ?"
+			`,
+			want: " var q = \"select `u`.`id`, `u`.`name` from `user` as `u`\" +" +
+				" \" join `post` as `p` on `u`.`id` = `p`.`user_id` where `u`.`id` = ?\"",
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			got := prs(tt.input)
 			if diff := cmp.Diff(tt.want, got); diff != "" {
-				t.Errorf("mismatch (-want +got) %s\n", diff)
+				t.Errorf("mismatch (-want +got) %v\n", diff)
 			}
 		})
 	}
